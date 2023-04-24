0bc0b3b0a4cf77febae52475e0f1d22e852695ff
kawan-lopes <pereirakawan07@gmail.com>
2023-04-24 12:26:14 -0300

    usando banco de dados no servidor da soacy

--- assets/conn.php
+++ assets/conn.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$database = "loja_de_carros";
-$username = "root";
-$password = "";
+$servername = "15.229.73.17:3306";
+$database = "carro";
+$username = "dev";
+$password = "[!_ph3d7DLi/-(lJ";
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
