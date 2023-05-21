0512341b1c61b5b7453c9d2b2a391b8c5f4eb2d5
kawan-lopes <pereirakawan07@gmail.com>
2023-05-21 13:29:56 -0300

    fixed

--- admin/index.php
+++ admin/index.php
@@ -17,7 +17,7 @@ session_start();
     $hora = date('H');
     if( $hora >= 5 && $hora <= 12 )
       return 'Bom dia' ;
-    else if ( $hora > 12 && $hora <=18  )
+    else if ( $hora > 12 && $hora <=18)
       return 'Boa tarde';
     else
       return 'Boa noite';
