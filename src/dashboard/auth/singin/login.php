7f6df6da56ebd59f2702c7b9ea29b7130ba843fc
kawan-lopes <pereirakawan07@gmail.com>
2023-05-28 21:08:59 -0300

    pagina de login e redirecionamento funcionando

--- src/dashboard/auth/singin/login.php
+++ src/dashboard/auth/singin/login.php
@@ -1,6 +1,6 @@
 <?php
 // carregando dependencias
-include_once('../../assets/conn.php'); // puxando arquivo de conexao com o banco de dados
+include_once($_SERVER['DOCUMENT_ROOT'].'/sistemadecarro/src/assets/php/conn.php'); // puxando arquivo de conexao com o banco de dados
 session_start(); // carregando sessoes
 
 // definindo variaveis
@@ -16,9 +16,9 @@ $db_user_arr = mysqli_fetch_array($db_user);
 // verificando se o banco de dados retornou que o user é valido
 if($db_user->num_rows != 0 or $db_user->num_rows != null){
     $_SESSION["userID"] = $db_user_arr["id_user"]; // criando session com id de usuario
-    header('Location: http://localhost/sistemadecarro/admin/'); // mandando o usuario para o painel de controle
+    header('Location: http://localhost/sistemadecarro/'); // mandando o usuario para o painel de controle
 }else{
-    header('Location: http://localhost/sistemadecarro/admin/pages/singin/?err=Erro ao fazer login');
+    header('Location: http://localhost/sistemadecarro/src/dashboard/pages/singin/?err=Erro ao fazer login');
      // caso os dados estejam errados, volta para pagina de login com um erro
 }
 
