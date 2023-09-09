5b4ba5b49f2701f9ccc7b048faa55b5f525eb25e
kawan <64042641+kawan-lopes@users.noreply.github.com>
2023-09-09 15:09:11 -0300

    Update email.php

--- src/dashboard/auth/singin/email.php
+++ src/dashboard/auth/singin/email.php
@@ -19,9 +19,6 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.zoho.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'contato@soacy.com'; 
-    $mail->Password   = 'email_soacy7971';                     //SMTP username
-    // $mail->Password   = 'nrzdewlnwiazocoq';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
